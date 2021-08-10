c7a2f1637148db491d97fdfe5abe55f81c73b1b1
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-10 21:54:17 +0530

    Largest of Number Pushed

---
+++ src/com/bridgelab/day5/LargestNumber.java
@@ -0,0 +1,20 @@
+package com.bridgelab.day5;
+
+public class LargestNumber {
+public static void main(String[] args) {
+	int num1=10,num2=50,num3=30;
+	
+	if(num1>num2 && num1>num3)
+	{
+		System.out.println(num1 + " is Greater");
+	}
+	else if(num2>num1 && num2> num3)
+	{
+		System.out.println(num2+ " is Greater");
+	}
+	else
+	{
+		System.out.println( num3+ " is Greater");
+	}
+}
+}
