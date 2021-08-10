2f66e1417c787262f18ad3afa4d96c9cde50914c
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-11 00:12:12 +0530

    PowerCode Pushed

--- src/com/bridgelab/day5/PowerCode.java
+++ src/com/bridgelab/day5/PowerCode.java
@@ -1,7 +1,20 @@
 package com.bridgelab.day5;
 
+
+import java.util.Scanner;
+
 public class PowerCode {
 public static void main(String[] args) {
+	int Number;
+	Scanner sc= new Scanner(System.in);
+	System.out.println("Enter the Number");
+	Number= sc.nextInt();
 	
+	int PowerOfNumber=1;
+	for (int i=1;i<=Number;i++)
+	{
+		PowerOfNumber= 2*Number;
+	}
+	System.out.println(PowerOfNumber);
 }
 }
