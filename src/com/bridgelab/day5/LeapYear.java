9f46e25890eb0c39b2fb5ef628a4fffacc4d708a
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-10 21:47:39 +0530

    LeapYear Code Pushed

---
+++ src/com/bridgelab/day5/LeapYear.java
@@ -0,0 +1,29 @@
+package com.bridgelab.day5;
+
+import java.util.Scanner;
+
+public class LeapYear {
+	public static void main(String[] args) {
+		int year;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the year");
+		year= sc.nextInt();
+		
+		if((year%4)  == 0)
+		{
+			if((year % 100) == 0 )
+			{
+				if((year %400) == 0)
+				{
+					System.out.println(year + " is a leap year");
+				}
+				else{System.out.println(year + " is a Not leap year");}
+			}else {
+			System.out.println(year + " is a  leap year");}
+		}
+		else {
+		System.out.println(year + " is a Not leap year");
+		}
+	}
+
+}
